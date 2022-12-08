06ce9f16271d7d8f999aad6a9cc4d957d9b5d2e0
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-08 15:39:47 +0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'home#index'
+  Rails.application.routes.draw do
+
+    root to: 'home#index'
+    namespace :api do 
+      namespace :v1 do 
+       resources :fruits, only: [:index, :create, :destroy, :update]
+      end 
+    end 
+  end
 end
 
