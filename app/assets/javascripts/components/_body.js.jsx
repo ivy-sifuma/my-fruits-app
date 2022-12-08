b19681af26dc29d6463cadf327ce2fc5fe51cf37
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-08 14:34:47 +0300

    Updated code

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -50,6 +50,111 @@ class Body extends React.Component {
         }  
     }
 
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.updateFruit = this.updateFruit.bind(this)
+  
+  handleUpdate(fruit){
+    fetch(`http://localhost:3000/api/v1/fruits/${fruit.id}`, 
+    {
+      method: 'PUT',
+      body: JSON.stringify({fruit: fruit}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => { 
+        this.updateFruit(fruit)
+      })
+  }
+  updateFruit(fruit){
+    let newFruits = this.state.fruits.filter((f) => f.id !== fruit.id)
+    newFruits.push(fruit)
+    this.setState({
+      fruits: newFruits
+    })
+  }
+
+ componentDidMount(){
+    fetch('/api/v1/items.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ items: data }) });
+  }
+render(){
+    return(
+      <div>
+        <NewItem handleSubmit = {this.handleSubmit} />
+        <AllItems items={this.state.items} handleDelete = {this.handleDelete} handleUpdate = {this.handleUpdate}/>
+      </div>
+    )
+  }
+
+
+
+const AllFruits = (props) => {
+var fruits = props.fruits.map((fruit) => {
+    return(
+      <div key={fruit.id}>
+       <Fruit fruit={fruit} handleDelete={props.handleDelete} handleUpdate={props.handleUpdate}/>
+      </div>
+    )
+  })
+
+}
+ 
+
+class Fruit extends React.Component{
+constructor(props){
+    
+    this.handleEdit = this.handleEdit.bind(this)
+  }
+handleEdit(){
+   if(this.state.editable){
+      let name = this.name.value
+      let description = this.description.value
+      let id = this.props.fruit.id
+      let fruit = {id: id, name: name, description: description}
+      this.props.handleUpdate(fruit)
+    }
+    this.setState({
+      editable: !this.state.editable
+    })
+  }
+  
+  render(){
+    let name = this.state.editable ? <input type='text' ref={input => this.name = input} defaultValue={this.props.fruit.name}/>:<h3>{this.props.fruit.name}</h3>
+    let description = this.state.editable ? <input type='text' ref={input => this.description = input} defaultValue={this.props.fruit.description}/>:<p>{this.props.fruit.description}</p>
+    return(
+      <div>
+        {name}
+        {description}
+        <button onClick={() => this.handleEdit()}>{this.state.editable? 'Submit' : 'Edit'}</button>
+        <button onClick={() => this.props.handleDelete(this.props.fruit.id)}>Delete</button>
+      </div>
+    )      
+  }
+}
+
+this.handleUpdate = this.handleUpdate.bind(this);
+    this.updateFruit = this.updateFruit.bind(this)
+  
+  handleUpdate(fruit){
+    fetch(`http://localhost:3000/api/v1/fruits/${fruit.id}`, 
+    {
+      method: 'PUT',
+      body: JSON.stringify({fruit: fruit}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => { 
+        this.updateFruit(fruit)
+      })
+  }
+  updateFruit(fruit){
+    let newFruits = this.state.fruits.filter((f) => f.id !== fruit.id)
+    newFruits.push(fruit)
+    this.setState({
+      fruits: newFruits
+    })
+
     componentDidMount(){
         fetch('/api/v1/fruits.json')
           .then((response) => {return response.json()})
