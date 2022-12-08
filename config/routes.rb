7c64d8a5ba5c56384b34a991282318205bfa7240
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-08 16:29:34 +0300

    Code Updated

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
 
     root to: 'home#index'
-    namespace :api do 
-      namespace :v1 do 
-       resources :fruits, only: [:index, :create, :destroy, :update]
-      end 
+  namespace :api do 
+    namespace :v1 do 
+     resources :fruits, only: [:index, :create, :destroy, :update]
     end 
-  
+  end 
+end
 
 
